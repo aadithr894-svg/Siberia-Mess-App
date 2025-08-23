9bbfd696eb2b116ac9dbac299f05cfb9cc11cc38
Aadith <aadithr894@gmail.com>
2025-08-23 13:17:53 +0530

    Added Config class and updated app.py for Render MySQL deployment

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'mydatabase')
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))  # default MySQL port
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 5432))  # default MySQL port
 
     # Flask secret key (set this in Render as well)
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
