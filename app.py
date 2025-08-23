869dc1b31cd09d325293a78c497d6940171225df
Aadith <aadithr894@gmail.com>
2025-08-23 13:49:28 +0530

    PostgreSQL-ready Flask app for Render

--- app.py
+++ app.py
@@ -974,6 +974,7 @@ def approve_late(late_mess_id):
 # ----------------- START APP -----------------
 # ----------------- START APP -----------------
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # fallback for local testing
+    app.run(host="0.0.0.0", port=port)
 
