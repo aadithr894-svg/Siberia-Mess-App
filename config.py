ea170ecbdc574755583848e0a1cac2a19a47f275
Aadith <aadithr894@gmail.com>
2025-09-01 15:22:57 +0530

    first commit

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 class Config:
     # MySQL configuration from Render environment variables
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'yamanote.proxy.rlwy.net')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mydb.cfc0uui6evlw.eu-north-1.rds.amazonaws.com')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'gpcMenvBLdOPPqHfbYDpXJPJiVlMXyaV')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'mess_app')
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 22110))  # default MySQL port
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Admin321')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'mydb')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))  # default MySQL port
 
     # Flask secret key (set this in Render as well)
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
